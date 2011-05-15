1331b783484159ae8f7d942f77dbb329c0a18917
Davide Guerri <davide.guerri@gmail.com>
2011-05-15 11:43:52 +0000

    adds support for traffic shaping *to be tested*

--- lib/os_captive_portals/linux_captive_portal.rb
+++ lib/os_captive_portals/linux_captive_portal.rb
@@ -352,16 +352,16 @@ class OsCaptivePortal
     execute_actions(firewall_paranoid_remove_user_actions, :blind => true)
     execute_actions(firewall_add_user_actions)
 
-    unless @total_upload_bandwidth.blank? or @upload_bandwidth.blank?
+    unless @total_upload_bandwidth.blank? or upload_bandwidth.blank?
       shaping_up_paranoid_remove_user_actions = [
         # upload class, qdisc and filter
-        "#{TC} class  del dev '#{@cp_interface}' parent 1:1 classid 1:#{mark} htb rate #{@upload_bandwidth}kbit ceil #{@upload_bandwidth}kbit burst 20k prio 1",
+        "#{TC} class  del dev '#{@cp_interface}' parent 1:1 classid 1:#{mark} htb rate #{upload_bandwidth}kbit ceil #{upload_bandwidth}kbit burst 20k prio 1",
         "#{TC} qdisc  del dev '#{@cp_interface}' parent 1:#{mark} handle #{mark}: sfq perturb 10",
         "#{TC} filter del dev '#{@cp_interface}' parent 1: prio 1 protocol ip handle #{mark} fw classid 1:#{mark}",
       ]
       shaping_up_add_user_actions = [
         # upload class, qdisc and filter
-        "#{TC} class  add dev '#{@cp_interface}' parent 1:1 classid 1:#{mark} htb rate #{@upload_bandwidth}kbit ceil #{@upload_bandwidth}kbit burst 20k prio 1",
+        "#{TC} class  add dev '#{@cp_interface}' parent 1:1 classid 1:#{mark} htb rate #{upload_bandwidth}kbit ceil #{upload_bandwidth}kbit burst 20k prio 1",
         "#{TC} qdisc  add dev '#{@cp_interface}' parent 1:#{mark} handle #{mark}: sfq perturb 10",
         "#{TC} filter add dev '#{@cp_interface}' parent 1: prio 1 protocol ip handle #{mark} fw classid 1:#{mark}",
       ]
@@ -373,13 +373,13 @@ class OsCaptivePortal
     unless @total_download_bandwidth.blank? or download_bandwidth.blank?
       shaping_down_paranoid_remove_user_actions = [
         # dowload class, qdisc and filter
-        "#{TC} class  del dev '#{@wan_interface}' parent 1:1 classid 1:#{mark} htb rate #{@download_bandwidth}kbit ceil #{@download_bandwidth}kbit burst 20k prio 1",
+        "#{TC} class  del dev '#{@wan_interface}' parent 1:1 classid 1:#{mark} htb rate #{download_bandwidth}kbit ceil #{download_bandwidth}kbit burst 20k prio 1",
         "#{TC} qdisc  del dev '#{@wan_interface}' parent 1:#{mark} handle #{mark}: sfq perturb 10",
         "#{TC} filter del dev '#{@wan_interface}' parent 1: prio 1 protocol ip handle #{mark} fw classid 1:#{mark}",
       ]
       shaping_down_add_user_actions = [
         # dowload class, qdisc and filter
-        "#{TC} class  add dev '#{@wan_interface}' parent 1:1 classid 1:#{mark} htb rate #{@download_bandwidth}kbit ceil #{@download_bandwidth}kbit burst 20k prio 1",
+        "#{TC} class  add dev '#{@wan_interface}' parent 1:1 classid 1:#{mark} htb rate #{download_bandwidth}kbit ceil #{download_bandwidth}kbit burst 20k prio 1",
         "#{TC} qdisc  add dev '#{@wan_interface}' parent 1:#{mark} handle #{mark}: sfq perturb 10",
         "#{TC} filter add dev '#{@wan_interface}' parent 1: prio 1 protocol ip handle #{mark} fw classid 1:#{mark}",
       ]
@@ -421,16 +421,16 @@ class OsCaptivePortal
 
     execute_actions(firewall_remove_user_actions)
 
-    unless @total_upload_bandwidth.blank? or @upload_bandwidth.blank?
+    unless @total_upload_bandwidth.blank? or upload_bandwidth.blank?
       shaping_up_paranoid_remove_user_actions = [
         # upload class, qdisc and filter
-        "#{TC} class  del dev '#{@cp_interface}' parent 1:1 classid 1:#{mark} htb rate #{@upload_bandwidth}kbit ceil #{@total_upload_bandwidth}kbit burst 20k prio 1",
+        "#{TC} class  del dev '#{@cp_interface}' parent 1:1 classid 1:#{mark} htb rate #{upload_bandwidth}kbit ceil #{upload_bandwidth}kbit burst 20k prio 1",
         "#{TC} qdisc  del dev '#{@cp_interface}' parent 1:#{mark} handle #{mark}: sfq perturb 10",
         "#{TC} filter del dev '#{@cp_interface}' parent 1: prio 1 protocol ip handle #{mark} fw classid 1:#{mark}",
       ]
       shaping_up_add_user_actions = [
         # upload class, qdisc and filter
-        "#{TC} class  add dev '#{@cp_interface}' parent 1:1 classid 1:#{mark} htb rate #{@upload_bandwidth}kbit ceil #{@total_upload_bandwidth}kbit burst 20k prio 1",
+        "#{TC} class  add dev '#{@cp_interface}' parent 1:1 classid 1:#{mark} htb rate #{upload_bandwidth}kbit ceil #{upload_bandwidth}kbit burst 20k prio 1",
         "#{TC} qdisc  add dev '#{@cp_interface}' parent 1:#{mark} handle #{mark}: sfq perturb 10",
         "#{TC} filter add dev '#{@cp_interface}' parent 1: prio 1 protocol ip handle #{mark} fw classid 1:#{mark}",
       ]
@@ -442,13 +442,13 @@ class OsCaptivePortal
     unless @total_download_bandwidth.blank? or download_bandwidth.blank?
        shaping_down_paranoid_remove_user_actions = [
          # dowload class, qdisc and filter
-         "#{TC} class  del dev '#{@wan_interface}' parent 1:1 classid 1:#{mark} htb rate #{@download_bandwidth}kbit ceil #{@total_download_bandwidth}kbit burst 20k prio 1",
+         "#{TC} class  del dev '#{@wan_interface}' parent 1:1 classid 1:#{mark} htb rate #{download_bandwidth}kbit ceil #{download_bandwidth}kbit burst 20k prio 1",
          "#{TC} qdisc  del dev '#{@wan_interface}' parent 1:#{mark} handle #{mark}: sfq perturb 10",
          "#{TC} filter del dev '#{@wan_interface}' parent 1: prio 1 protocol ip handle #{mark} fw classid 1:#{mark}",
        ]
        shaping_down_add_user_actions = [
          # dowload class, qdisc and filter
-         "#{TC} class  add dev '#{@wan_interface}' parent 1:1 classid 1:#{mark} htb rate #{@download_bandwidth}kbit ceil #{@total_download_bandwidth}kbit burst 20k prio 1",
+         "#{TC} class  add dev '#{@wan_interface}' parent 1:1 classid 1:#{mark} htb rate #{download_bandwidth}kbit ceil #{download_bandwidth}kbit burst 20k prio 1",
          "#{TC} qdisc  add dev '#{@wan_interface}' parent 1:#{mark} handle #{mark}: sfq perturb 10",
          "#{TC} filter add dev '#{@wan_interface}' parent 1: prio 1 protocol ip handle #{mark} fw classid 1:#{mark}",
        ]
