0a893c453b5c35f00b44cdf085e74c1feff97303
Matteo Latini <mtylty@gmail.com>
2011-10-18 09:26:11 +0000

    adds pids directory creation

--- lib/recipes/rails.rb
+++ lib/recipes/rails.rb
@@ -2,7 +2,7 @@ namespace :rails do
   desc "Repair permissions to allow user to perform all actions"
   task :repair_permissions, :roles => :app do
     puts "Applying correct permissions to allow for proper command execution"
-    try_sudo "mkdir -p #{shared_path}/log #{current_path}/tmp #{shared_path}/system"
+    try_sudo "mkdir -p #{shared_path}/log #{current_path}/tmp #{shared_path}/system #{shared_path}/pids"
     try_sudo "chmod -R 770 #{shared_path}/log #{current_path}/tmp #{shared_path}/system"
     try_sudo "chown -R www-data.www-data #{shared_path}/log #{current_path}/tmp #{shared_path}/system"
   end
