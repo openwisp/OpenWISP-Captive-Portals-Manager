35448c3e8add145df20b847428015312a9dc6768
nemesisdesign <federico.capoano@teletu.it>
2014-05-06 10:50:54 +0200

    avoid "undefined method `[]' for nil:NilClass" in linux_captive_portal

--- lib/os_captive_portals/linux_captive_portal.rb
+++ lib/os_captive_portals/linux_captive_portal.rb
@@ -522,7 +522,9 @@ class OsCaptivePortal
     if is_ipv4_address?(client_address)
       up_match = /\A\s*(\d+)\s+(\d+)\s+/.match(%x[#{IPTABLES} -t mangle -vnx -L '_AUTH_IN_#{@cp_interface}' | grep '#{client_address}'])
       dn_match = /\A\s*(\d+)\s+(\d+)\s+/.match(%x[#{IPTABLES} -t mangle -vnx -L '_AUTH_OUT_#{@cp_interface}' | grep '#{client_address}'])
-      ret = [up_match[2].to_i, dn_match[2].to_i]
+      if !up_match[2].nil? and !dn_match[2].nil?
+        ret = [up_match[2].to_i, dn_match[2].to_i]
+      end
     elsif is_ipv6_address?(client_address)
       #TO DO: ip6tables rules!
       not_implemented
@@ -541,7 +543,9 @@ class OsCaptivePortal
     if is_ipv4_address?(client_address)
       up_match = /\A\s*(\d+)\s+(\d+)\s+/.match(%x[#{IPTABLES} -t mangle -vnx -L '_AUTH_IN_#{@cp_interface}' | grep '#{client_address}'])
       dn_match = /\A\s*(\d+)\s+(\d+)\s+/.match(%x[#{IPTABLES} -t mangle -vnx -L '_AUTH_OUT_#{@cp_interface}' | grep '#{client_address}'])
-      ret = [up_match[1].to_i, dn_match[1].to_i]
+      if !up_match[1].nil? and !dn_match[1].nil?
+        ret = [up_match[1].to_i, dn_match[1].to_i]
+      end
     elsif is_ipv6_address?(client_address)
       #TO DO: ip6tables rules!
       not_implemented
