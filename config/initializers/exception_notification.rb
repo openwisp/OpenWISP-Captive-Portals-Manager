32380c4b1b7bb99c87943d78cca14c20fc934d87
nemesisdesign <federico.capoano@teletu.it>
2014-09-18 11:19:44 +0200

    Added :ignore_if section in exception notification initializer

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,13 +1,16 @@
 ExceptionNotifier::Notifier.prepend_view_path File.join(Rails.root, 'app/views')
 
-recipients = CONFIG['exception_notification_recipients'].split(',') rescue 'root@localhost'
+recipients = CONFIG['exception_notification_recipients'].split(',') rescue false
 sender = CONFIG['exception_notification_sender'] or 'owcpm@localhost'
 email_subject_prefix = CONFIG['exception_notification_prefix'] or 'OWCPM'
 
-Railscp::Application.config.middleware.use(
-  ExceptionNotifier,
-  :email_prefix => email_subject_prefix << ' ',
-  :sender_address => sender,
-  :exception_recipients => recipients,
-  :sections =>  %w(request session environment backtrace)
-)
+if recipients
+  Railscp::Application.config.middleware.use(
+    ExceptionNotifier,
+    :email_prefix => email_subject_prefix << ' ',
+    :sender_address => sender,
+    :exception_recipients => recipients,
+    :sections =>  %w(request session environment backtrace),
+    :ignore_if => lambda { |env, e| e.message =~ /^to_sym/ }
+  )
+end
